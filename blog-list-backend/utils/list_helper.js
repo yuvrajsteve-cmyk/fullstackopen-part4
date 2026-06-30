a2068e6dda301c7b76e292c4c17d810e536ccb33
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-30 18:13:57 +0530

    4.4: Helper Functions and Unit Tests, step 2

--- blog-list-backend/utils/list_helper.js
+++ blog-list-backend/utils/list_helper.js
@@ -4,6 +4,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.lenght === 0) return 0
+
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
