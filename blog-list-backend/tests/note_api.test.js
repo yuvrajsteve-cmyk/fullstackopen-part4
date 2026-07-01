d34b165a48da21fa6f975b920d600abce41f4d44
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-01 15:47:00 +0530

    test pass

--- blog-list-backend/tests/note_api.test.js
+++ blog-list-backend/tests/note_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Note = require('../models/note')
+const assert = require('assert')
 
 const api = supertest(app)
 
@@ -13,8 +14,18 @@ beforeAll(async () => {
   }
 })
 
+const initialNotes = [
+  { content: 'HTML is easy', important: false },
+  { content: 'Browser can execute only JavaScript', important: true }
+]
+
 beforeEach(async () => {
   await Note.deleteMany({})
+
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
 }, 30000)
 
 test('notes are returned as json', async () => {
@@ -22,6 +33,19 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test.only('notes are returned as json', async () => {
+  await api
+  .get('/api/notes')
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+})
+
+test.only('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 2)
 })
 
 afterAll(async () => {
