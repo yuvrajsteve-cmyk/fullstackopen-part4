402d89a11b4e1a7958ab3c68a8b3057a64802278
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-02 10:57:44 +0530

    4.8: Blog List Tests, step 1

--- blog-list-backend/controllers/blogs.js
+++ blog-list-backend/controllers/blogs.js
@@ -1,15 +1,31 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
+
+// blogsRouter.get('/', async (request, response) => {
+//   const blogs = await Blog.find({})
+//   response.json(blogs)
+// })
+
+// blogsRouter.post('/', async (request, response) => {
+//   const blog = new Blog(request.body)
+//   const savedBlog = await blog.save()
+//   response.status(201).json(savedBlog)
+// })
+
+// add new blog router for exercise with promise based
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+
+// add new blog router for exercise with async / await based
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs)
-})
-
-blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+  response.status(200).json(blogs)
 })
 
 module.exports = blogsRouter
--- blog-list-backend/tests/blog_api.test.js
+++ blog-list-backend/tests/blog_api.test.js
@@ -1,23 +1,21 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-
-
-console.log('TEST_MONGODB_URI:', process.env.TEST_MONGODB_URI) 
-
-
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../app')
+const app = require('../app') 
+const api = supertest(app)
 const Blog = require('../models/blog')
 
-const api = supertest(app)
-
-
-jest.setTimeout(30000)
+const initialBlogs = [
+  { title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7 },
+  { title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/', likes: 5 }
+]
 
 beforeAll(async () => {
   await mongoose.connect(process.env.TEST_MONGODB_URI)
-  console.log('Mongoose state:', mongoose.connection.readyState) 
+})
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -27,6 +25,11 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
 afterAll(async () => {
-  await mongoose.connection.close()
+  await mongoose.connection.close() 
 })
